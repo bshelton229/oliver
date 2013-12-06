a4b880591e09c48d95902aec43327558c6e0f90d
Bryan Shelton <bryan@sheltonplace.com>
2013-12-06 15:04:48 -0800

    Menu generation.

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -1,5 +1,25 @@
 module BootstrapHelper
+  # Define the main navigation links
+  def navbar_items
+    [
+      {
+        name: 'Users',
+        link: users_path,
+        active: -> { controller_name == 'users' }
+      }
+    ]
+  end
+
+  # Generate the navbar <ul>
   def navbar
-    ""
+    content_tag :ul, navbar_items.map { |item| navbar_link(item[:name], item[:link], item[:active]) }.join.html_safe, class: 'nav navbar-nav'
+  end
+
+  # Generate a single navbar <li>
+  def navbar_link(name, path, active=false)
+    if active.kind_of?(Proc)
+      active = active.call
+    end
+    content_tag(:li, link_to(name, path), class: active ? 'active' : '')
   end
 end
