fd178bbc96d1c156ce8bf55a55f1a1ca10adc9c4
Bryan Shelton <bryan@sheltonplace.com>
2013-12-06 14:18:03 -0800

    User specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,20 @@ describe User do
     user = create(:user)
     expect(user).to be_valid
   end
+
+  it 'Should validate password length' do
+    user = User.new email: 'test@user.com', password: '1234567'
+    expect(user).to_not be_valid
+    expect(user.errors[:password].first).to match %r{too short}
+
+    user.password = '12345678'
+
+    expect(user).to be_valid
+    expect(user.save).to be_true
+  end
+
+  it 'Should allow phrase passwords' do
+    user = create(:user, password: 'eat your spinach')
+    expect(user).to be_persisted
+  end
 end
