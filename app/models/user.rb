dd2f6b8f3b5db5caa91d471a67fdfc8f11f276bf
Bryan Shelton <bryan@sheltonplace.com>
2013-12-06 14:26:34 -0800

    Default user scope.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+
+  default_scope -> { order('email ASC') }
 end
