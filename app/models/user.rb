19f4a367293a8e1817eb3d974af1c9da57540a9b
Bryan Shelton <bryan@sheltonplace.com>
2013-12-06 17:56:51 -0800

    Show user last login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   default_scope -> { order('email ASC') }
+
+  def last_login(format=:long)
+    last_sign_in_at.present? ? last_sign_in_at.to_s(format) : nil
+  end
 end
