1bec1c1e9631a5c7622cef13d1d3288dda03910a
Bryan Shelton <bryan@sheltonplace.com>
2013-12-13 17:37:21 -0800

    Page_header helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Produces a page header with a new link
+  def page_header(title,new_link)
+    content_tag :div, class: 'page-header' do
+      content_tag :h1 do
+        (title + link_to(fa_icon('plus')+' New', new_link, class: 'pull-right btn btn-primary header-new-btn')).html_safe
+      end
+    end
+  end
 end
